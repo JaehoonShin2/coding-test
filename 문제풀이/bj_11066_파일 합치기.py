9476e0d6641f0b8255e143a6e8110c2d9295bd32
JaehoonShin2 <96221394+JaehoonShin2@users.noreply.github.com>
2022-12-15 16:33:38 +0900

    commit

--- 문제풀이/bj_11066_파일 합치기.py
+++ 문제풀이/bj_11066_파일 합치기.py
@@ -9,15 +9,33 @@ for _ in range(t):
     n = int(sys.stdin.readline().rstrip())
     arr = list(map(int, sys.stdin.readline().split()))
 
-    dp = [[0 for i in range(n+1)] for _ in range(n+1)]
-
+    #누적합을 담기 위한 리스트
     sum = [0 for _ in range(n+1)]
 
     for i in range(n):
         sum[i+1] = sum[i] + arr[i]
     
+    dp = [[0 for i in range(n+1)] for _ in range(n+1)] #2차원리스트 선언
+
+    # 2개 연속의 합, 3개 연속의 합.... n개 연속의 합, 즉 arr에 대한 총 갯수의 합
     for i in range(2, n+1):
+        # 연속의 합을 구할 때 1~2, 2~3, 3~4, 1~3, 2~4, 1~4.. 와 같이 인덱스 값을 부여할 변수
         for j in range(1, n+2-i):
-            dp[j][j+i-1] = min([dp[j][j+k] + dp[j+k+1][j+i-1] for k in range(i-1)]) + (sum[j+i-1] - sum[j-1])
+            dp[j][j+i-1] = min([dp[j][j+k] + dp[j+k+1][j+i-1] for k in range(i-1)]) + (sum[j+i-1] - sum[j-1])   
 
     print(dp[1][n])
+
+    
+
+    # dp = [[0 for i in range(n+1)] for _ in range(n+1)]
+
+    # sum = [0 for _ in range(n+1)]
+
+    # for i in range(n):
+    #     sum[i+1] = sum[i] + arr[i]
+    
+    # for i in range(2, n+1):
+    #     for j in range(1, n+2-i):
+    #         dp[j][j+i-1] = min([dp[j][j+k] + dp[j+k+1][j+i-1] for k in range(i-1)]) + (sum[j+i-1] - sum[j-1])
+
+    # print(dp[1][n])
---
+++ 문제풀이/bj_2108_통계학.py
@@ -0,0 +1,51 @@
+import sys
+from queue import PriorityQueue
+
+sys.stdin = open('C:/Users/R/Desktop/python_input/bj_2108.txt')
+
+# 산술평균 : N개의 수들의 합을 N으로 나눈 값
+# 중앙값 : N개의 수들을 증가하는 순서로 나열했을 경우 그 중앙에 위치하는 값
+# 최빈값 : N개의 수들 중 가장 많이 나타나는 값
+# 범위 : N개의 수들 중 최댓값과 최솟값의 차이
+
+n = int(sys.stdin.readline().rstrip())
+nums = [int(sys.stdin.readline().rstrip()) for _ in range(n)]
+
+print(round(sum(nums)/n))
+
+nums.sort()
+print(nums[int((n-1)/2)])
+
+
+# 최빈값
+counts = dict()
+for i in range(1,n+1) :
+	counts[i] = []
+
+maxCount = 1
+count = 1
+for j in range(1,n) :
+	if nums[j] == nums[j-1] :
+		count += 1
+	else :
+		counts[count].append(nums[j-1])
+		if maxCount < count: maxCount = count
+		count = 1
+	if j == n-1 : 
+		counts[count].append(nums[j])
+		if maxCount < count : maxCount = count
+
+if n == 1 :
+	counts[1].append(nums[0])
+
+counts[maxCount].sort()
+if len(counts[maxCount]) == 1 :
+	print(counts[maxCount][0])
+else :
+	print(counts[maxCount][1])
+
+
+print(nums[-1]-nums[0])
+
+
+
