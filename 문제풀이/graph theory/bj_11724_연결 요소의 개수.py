560615fe47eb7d6348b0dace768d60a2b05b9831
JaehoonShin2 <96221394+JaehoonShin2@users.noreply.github.com>
2022-12-28 16:27:34 +0900

    commit

--- 문제풀이/graph theory/bj_11724_연결 요소의 개수.py
+++ 문제풀이/graph theory/bj_11724_연결 요소의 개수.py
@@ -1,5 +1,6 @@
 import sys
 sys.setrecursionlimit(10**6)
+from collections import deque
 
 sys.stdin = open('C:/Users/R/Desktop/python_input/bj_11724.txt')
 
@@ -11,8 +12,9 @@ for i in range(m):
     graph[a].append(b)
     graph[b].append(a)
 
-visited = [False] * (n+1)
+D_visited = [False] * (n+1)
 
+## dfs 해결법
 def dfs(v, visited):
     if visited[v] == True: 
         return
@@ -23,11 +25,37 @@ def dfs(v, visited):
         else: continue
     return
 
-cnt = 0
+D_cnt = 0
 
 for i in range(1, n+1):
-    if visited[i] == False:
-        dfs(i, visited)
-        cnt += 1
+    if D_visited[i] == False:
+        dfs(i, D_visited)
+        D_cnt += 1
         
-print(cnt)
+print(D_cnt)
+
+B_visited = [False] * (n+1)
+
+def bfs(v, visited):
+    q = deque()
+    visited[v] = True
+    q.append(graph[v])
+    while q:
+        print(q)
+        now = q.popleft()
+        for node in now:
+            if visited[node] == False:
+                visited[node] = True
+                q.append(graph[node])
+            else: continue
+        return
+
+B_cnt = 0
+
+for i in range(1, n+1):
+    if B_visited[i] == False:
+        bfs(i, B_visited)
+        B_cnt += 1
+        
+print(B_cnt)
+
