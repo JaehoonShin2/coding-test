ef4250f55a7486260d1c9086c5ba62b707fd2235
JaehoonShin2 <96221394+JaehoonShin2@users.noreply.github.com>
2022-12-28 22:34:35 +0900

    commit

--- 문제풀이/graph theory/bj_1697_숨바꼭질.py
+++ 문제풀이/graph theory/bj_1697_숨바꼭질.py
@@ -5,32 +5,20 @@ sys.stdin = open('C:/Users/R/Desktop/python_input/bj_1697.txt')
 
 n, k = map(int, sys.stdin.readline().split())
 
-steps = [-1, 1, 2]
-distance = [int(1e9)] * 100001
+distance = [0] * 100001
 
 def bfs(v, target):
     q = deque()
     q.append(v)
-    distance[v] = 0
     while q:
-        now = q.popleft()
-        if now == target:
-            return distance[now]
-        for step in range(3):
-            if step == 2:
-                node = now * steps[step]
-            else :
-                node = now + steps[step]
-
-            if v < target : # 1, 1000
-                if node > target+1 or 0 > node: continue
-            if v > target : # 1000, 1
-                if node > v or 0 > node:  continue
-
-            if distance[node] > distance[now] and distance[node] == 1e9:
-                distance[node] = min(distance[node], distance[now] + 1)
-                q.append(node)
-            else : continue
+        x = q.popleft()
+        if x == target:
+            print(distance[x])
+            return
+        for nx in ( x-1, x+1, x*2):
+            if 0 <= nx < 100001 and distance[nx] == 0 :
+                distance[nx] = distance[x] + 1
+                q.append(nx)
 
 
-print(bfs(n, k))
+bfs(100000, 1)
