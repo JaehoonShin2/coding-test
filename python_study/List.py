2a5a3a7a8d425976cd1cda2378ea40d1055f46fe
sjh <zwielicht2@gmail.com>
2023-03-25 23:16:18 +0900

    0325

--- python_study/List.py
+++ python_study/List.py
@@ -1,12 +1,19 @@
 # 리스트(List) vs 딕셔너리(Dictionary) vs 집합(Set)
 #
 # 리스트는 mutable, 
-#
+# 선언방식
+list_1 = list()
+list_2 = []
+
 # 
 # 딕셔너리는 key:value 형태로 이루어진, 일종의 map 형태라고 보면 될 것 같다.
 # value 는 중복이 가능하지만, key 는 중복이 불가능하다. 
 # 만약 같은 key 값에 대해 값이 주어진다면, 나중에 선언된 value 값이 덮어씌워진다.
+dict = dict()
+dict = {}
 # 
-# 
+# 집합(set) 의 경우는 중복이 허용되지 않는, java에서의 set과 동일하다.
+set = ()
+
 
 
