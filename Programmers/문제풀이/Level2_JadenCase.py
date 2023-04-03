6510b7fdb98b60a5f5aa3f7228229766759299f2
sjh <zwielicht2@gmail.com>
2023-04-03 22:24:59 +0900

    commit

---
+++ Programmers/문제풀이/Level2_JadenCase.py
@@ -0,0 +1,20 @@
+
+input = open('../python_input/Level2_JadenCase.txt')
+input_str = input.readline().rstrip()
+# def solution(s):
+    
+#     answer = ''
+    
+#     sList = list(map(str, s.split(' ')))
+    
+#     for node in sList:
+#         if ord(node[0]) >= 48 and ord(node[0]) < 58:
+#             print(node[0])
+#             continue
+#         else: node[0].upper()
+        
+#         answer += node+' '
+        
+#     return answer
+
+# solution(input_str)
