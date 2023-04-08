02f5c1ec7de54c340d78f1e20aae5d016dbbd94b
sjh <zwielicht2@gmail.com>
2023-04-08 23:44:12 +0900

    commit

---
+++ Programmers/문제풀이/Level1_공원 산책_BFS.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+def solution(park, routes):
+    
+    # 주어진 방향으로 이동할 때 공원을 벗어나는지 확인합니다.
+    # 주어진 방향으로 이동 중 장애물을 만나는지 확인합니다.
+    # 위 두 가지중 어느 하나라도 해당된다면, 로봇 강아지는 해당 명령을 무시하고 다음 명령을 수행합니다.
+    
+    answer = []
+
+    
+    return answer
+
+park1 = ["SOO","OOO","OOO"]
+routes1 = ["E 2","S 2","W 1"]
+park = ["OSO","OOO","OXO","OOO"]
+routes = ["E 2","S 3","W 1"]
+
+
+print(solution(park1, routes1))
