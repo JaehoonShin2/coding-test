7a26185d9184c511c05f6018720eb900fab69041
sjh <zwielicht2@gmail.com>
2023-04-22 23:16:25 +0900

    commit

--- Programmers/문제풀이/Level2_피로도.py
+++ Programmers/문제풀이/Level2_피로도.py
@@ -1,7 +1,25 @@
-def solution(k, dungeons):
-    answer = -1
-    return answer
+from itertools import permutations as ps
 
+def solution(k, dungeons):
+    answer = 0
+
+    d_l = len(dungeons)
+    for p in ps(range(d_l)):
+        print('p : {}'.format(p))
+        tk = k
+        for idx, v in enumerate(p):
+            
+            if tk < dungeons[idx][0]:
+                answer = max(answer, idx)
+                break
+            
+            tk -= dungeons[v][1]
+            
+        else: 
+            return d_l
+            
+    return answer
+    
 
 k=80
 dungeons=[[80,20],[50,40],[30,10]]
