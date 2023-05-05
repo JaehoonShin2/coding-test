45836f4b0a47049e8debfb33d29b2de790085a8e
sjh <zwielicht2@gmail.com>
2023-05-05 23:53:20 +0900

    commit

--- Programmers/문제풀이/고득점 kit/hash/Level1_폰켓몬.py
+++ Programmers/문제풀이/고득점 kit/hash/Level1_폰켓몬.py
@@ -1,3 +1,12 @@
+from collections import Counter
 def solution(nums):
-    answer = 0
-    return answer
+    c = len(nums)/2
+    counter = Counter(nums)
+    max_c = len(counter)    
+    if max_c <= c:
+        return max_c
+    else:
+        return c
+
+nums=[3,3,3,2,2,4]
+print(solution(nums))
