96dfa3b01eef91e72aa1f9b4b31232bf91adab74
sjh <zwielicht2@gmail.com>
2023-05-14 22:37:23 +0900

    230514

--- Programmers/문제풀이/고득점 kit/Greedy/Level1_체육복.py
+++ Programmers/문제풀이/고득점 kit/Greedy/Level1_체육복.py
@@ -2,12 +2,28 @@ def solution(n, lost, reserve):
     
     n_dict = dict.fromkeys(range(1, n+1), 1)
     
-    print(n_dict)   
-    
     for l in lost:
         n_dict[l] -= 1
     
-    print(n_dict)
+    for r in reserve:
+        n_dict[r] += 1
+    
+    for k, v in n_dict.items():
+        if v == 2:
+            if k > 1 and n_dict[k-1] == 0:
+                n_dict[k-1] += 1
+                n_dict[k] -= 1
+            elif k < len(n_dict) and n_dict[k+1] == 0:
+                n_dict[k+1] += 1
+                n_dict[k] -= 1
+    
+    answer = n
+    for k, v in n_dict.items():
+        if v == 0:
+            answer -= 1
+    
+    return answer
+    
     
 n=5	
 lost=[2, 4]
