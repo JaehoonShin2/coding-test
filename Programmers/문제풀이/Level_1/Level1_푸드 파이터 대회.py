e41e668b1f0241ebd4cd74f420a7e9322b99f06c
sjh <zwielicht2@gmail.com>
2023-05-16 16:25:43 +0900

    230516

---
+++ Programmers/문제풀이/Level_1/Level1_푸드 파이터 대회.py
@@ -0,0 +1,13 @@
+def solution(food):
+    answer = ['0']
+    for i in reversed(range(len(food))):
+        f = food[i] // 2
+        if f > 0:
+            new_f = f * str(i)
+            answer.insert(0, new_f)
+            answer.append(new_f)
+        
+    return ''.join(answer)
+
+food=[1, 7, 1, 2]
+print(solution(food))
