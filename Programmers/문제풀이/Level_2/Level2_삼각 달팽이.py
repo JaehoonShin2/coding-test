27c53bc9d2f9d3e13f33a1d0e6d703bbdb8a97ec
sjh <zwielicht2@gmail.com>
2023-05-31 21:53:24 +0900

    230531

--- Programmers/문제풀이/Level_2/Level2_삼각 달팽이.py
+++ Programmers/문제풀이/Level_2/Level2_삼각 달팽이.py
@@ -1,7 +1,27 @@
 def solution(n):
-    answer = [[0] * n for _ in range(n)]
+    answer = [[0] * (n+1) for i in range(n+1)]
     
-    return answer
-
+    x, y = -1, 0 # 좌표 초기화 => 처음 시작은 아래로 내려가기 때문에 x = -1
+    num = 1
+ 
+    for i in range(n): # 방향
+        for _ in range(i, n): # 좌표 구하기
+            if i % 3 == 0: # 하
+                x += 1
+            elif i % 3 == 1: # 우
+                y += 1
+            else: # 상
+                x -= 1
+                y -= 1
+            answer[x][y] = num
+            num += 1
+    
+    ans = []
+    for a in answer:
+        for b in a:
+            if b != 0:
+                ans.append(b)
+                
+    return ans
 n=5
 print(solution(n))
