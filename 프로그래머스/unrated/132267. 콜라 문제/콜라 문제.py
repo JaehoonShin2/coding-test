d438bfe4827e4e2061669600c42af85059111508
JaehoonShin2 <96221394+JaehoonShin2@users.noreply.github.com>
2023-07-27 22:00:37 +0900

    [unrated] Title: 콜라 문제, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
@@ -1,24 +1,12 @@
 def solution(a, b, n):
-    # 총 받을 수 있는 콜라의 갯수
+    # 최종적으로 받을 수 있는 콜라의 갯수
     answer = 0
-    # 해당 시점에서 남은 콜라의 갯수
-    temp = 0
-    # 돌려받은 콜라의 갯수+남은 콜라의 갯수가 a보다 작아질 때까지
-    while (n + temp) >= a:
-        # 만약 나누고 남은 콜라가 있고, n%a != 이라면
-        if n % a != 0 and temp > 0 :
-            n += temp
-            temp = 0
-        
-        # 현재의 콜라에서의 남을 갯수
-        temp = temp + (n % a)
-        
-        # c 는 n 개에서 a개만큼 나눈 값
-        c = n // a
-        
-        # b개만큼 돌려줄 떄, c번 콜라를 가져다줌
-        n = b*c
-        
-        answer += n
-    
+    # 현재 가진 콜라의 갯수가 a보다 크다면 반복
+    while n >= a:
+        # temp 는 n에서 a를 나눈 몫 * b개
+        temp = (n // a) * b
+        # answer 에 temp 를 더해줌
+        answer += temp
+        # 새로운 n는 돌려받은 temp 개 + 나누어떨어지지 않아 남은 갯수
+        n = temp + (n % a)
     return answer
