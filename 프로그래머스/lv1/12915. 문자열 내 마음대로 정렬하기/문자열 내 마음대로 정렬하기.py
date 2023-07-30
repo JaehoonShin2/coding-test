f22004f5e6a75ceaa8c3e633911651f440d818de
JaehoonShin2 <96221394+JaehoonShin2@users.noreply.github.com>
2023-07-30 22:40:20 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
@@ -2,19 +2,4 @@ from collections import defaultdict as dd
 
 def solution(strings, n):
     
-    answer = []
-    
-    temp_dic = dd(list)
-    
-    for s in strings:
-        temp_dic[s[n]].append(s)
-        
-    # 딕셔너리의 키 정렬
-    temp_key = [k for k in temp_dic.keys()]
-    temp_key.sort()
-    
-    for tk in temp_key:
-        temp_dic[tk].sort()
-        answer += temp_dic[tk]
-    
-    return answer
+    return sorted(sorted(strings), key=lambda x: x[n])
