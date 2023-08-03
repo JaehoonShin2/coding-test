af5dc01ffc8b3ac8b73eabdee331bd9b2bafab2c
sjh <zwielicht2@gmail.com>
2023-08-03 23:17:51 +0900

    230803

---
+++ Programmers/문제풀이/Level_1/Level1_크기가 작은 문자엻.py
@@ -0,0 +1,18 @@
+def solution(t, p):
+    answer = 0
+    
+    # 전체 문자열의 길이
+    n = len(t)
+    # 부분문자열의 길이
+    k = len(p)
+
+    # 반복문을 돌면서 t[i:i+k] 의 값이 p보다 작을 경우 answer에 1을 더한다.
+    for i in range(n+1-k):
+        if t[i:i+k] <= p:
+            answer += 1
+    
+    return answer
+
+t="10203"
+p="15"
+print(solution(t, p))
