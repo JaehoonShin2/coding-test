ad332147aff6f4a904751f910b376e27944ac643
JaehoonShin2 <96221394+JaehoonShin2@users.noreply.github.com>
2023-08-03 22:41:30 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
@@ -1,18 +1,14 @@
 def solution(t, p):
     answer = 0
     
-    # 부분문자열의 제한 길이
-    l = len(p)
-    
-    sub = ""
-    for i in range(len(t)):
-        # 만약 sub 의 길이가 l보다 작다면 추가
-        if len(sub) < l:
-            sub += t[i]
-        # 만약 길이가 동일하다면 우선 크기 비교를 한 뒤 앞자리를 팝하기
-        if len(sub) == l:
-            if sub <= p:
-                answer += 1
-            sub = sub[1:]
+    # 전체 문자열의 길이
+    n = len(t)
+    # 부분문자열의 길이
+    k = len(p)
+
+    # 반복문을 돌면서 t[i:i+k] 의 값이 p보다 작을 경우 answer에 1을 더한다.
+    for i in range(n+1-k):
+        if t[i:i+k] <= p:
+            answer += 1
     
     return answer
