83cf04fb72edfadeaa2eed08f5c14f97d9708228
sjh <zwielicht2@gmail.com>
2023-08-07 23:41:04 +0900

    230807

---
+++ Programmers/문제풀이/Level_1/Level1_가장 가까운 같은 글자.py
@@ -0,0 +1,13 @@
+from collections import defaultdict as dd
+def solution(s):
+    answer = []
+    
+    map = dd(list)
+    
+    for k, v in zip(s, range(len(s))):
+        answer.append(-1) if len(map[k]) == 0 else answer.append(v-map[k][-1])
+        map[k].append(v)
+    return answer
+
+s="banana"
+print(solution(s))
