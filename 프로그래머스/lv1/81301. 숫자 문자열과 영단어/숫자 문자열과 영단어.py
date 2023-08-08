63d114c589420625880f55f9501a5f83f98c3708
JaehoonShin2 <96221394+JaehoonShin2@users.noreply.github.com>
2023-08-08 23:06:48 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,18 +1,11 @@
 def solution(s):
     
-    temp = ""
-    answer = ""
+    answer = s
     
-    map = {"zero":0,"one":1, "two":2, "three":3, "four":4,
-           "five":5, "six":6, 'seven':7, "eight":8, "nine":9}
+    map = {"zero":'0',"one":'1', "two":'2', "three":'3', "four":'4',
+           "five":'5', "six":'6', 'seven':'7', "eight":'8', "nine":'9'}
     
-    for c in s:
-        if c.isnumeric():
-            answer += c
-        else:
-            temp += c
-            if map.get(temp) != None:
-                answer += str(map[temp])
-                temp = ""
+    for k, v in map.items():
+        answer = answer.replace(k, v)
     
     return int(answer)
