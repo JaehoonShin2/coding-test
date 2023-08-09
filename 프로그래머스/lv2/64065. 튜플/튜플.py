5928de00d5b075cc12c12e5319e0fdb01116df35
JaehoonShin2 <96221394+JaehoonShin2@users.noreply.github.com>
2023-08-09 23:02:46 +0900

    [level 2] Title: 튜플, Time: 0.11 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.py
+++ 프로그래머스/lv2/64065. 튜플/튜플.py
@@ -1,20 +1,8 @@
-from collections import defaultdict as dd
+from collections import Counter
 import re
 
 def solution(s):
-    
-    num_dic = dd(int)
-    
-    split_s = s.split('{')
 
-    sss = [ss.split(',') for ss in split_s if ss != '']
-     
-    for sssss in sss:
-        for sssssss in sssss:
-            if sssssss != '':
-                aa = re.sub('[^0-9]', '', sssssss)
-                num_dic[aa] += 1    
-
-    answer = sorted(num_dic, key=lambda x:num_dic[x], reverse=True)
-    
+    num_dic = Counter(re.findall('\d+', s))
+    answer = sorted(num_dic, key=lambda x:num_dic[x], reverse=True) 
     return list(map(int, answer))
