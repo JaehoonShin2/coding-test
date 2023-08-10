a774764e2602a24c552424f6d89d7c36b9ca15f4
JaehoonShin2 <96221394+JaehoonShin2@users.noreply.github.com>
2023-08-10 23:34:47 +0900

    [level 1] Title: 실패율, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42889. 실패율/실패율.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+def solution(N, stages):
+    
+    c = Counter(stages)
+    stage_failure_rate = {}
+    
+    for i in range(1, N+1):
+        stage_player = sum(v for k, v in c.items() if k >= i)
+        if stage_player > 0:
+            stage_failure_rate[i] = c[i]/stage_player
+        else:
+            stage_failure_rate[i] = 0
+    
+    answer = sorted(sorted(stage_failure_rate), key=lambda x:stage_failure_rate[x], reverse=True)
+    return list(map(int, answer))
