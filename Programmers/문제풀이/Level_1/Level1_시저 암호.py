f198efaeb8b3417961c3cad255f9c45424e4f5e8
sjh <zwielicht2@gmail.com>
2023-11-03 00:00:13 +0900

    231102

---
+++ Programmers/문제풀이/Level_1/Level1_시저 암호.py
@@ -0,0 +1,17 @@
+def solution(s, n):
+    answer = ''
+    for c in s:
+        if c != ' ':
+            i = ord(c)
+            if i > 96 and i+n>122:
+                i -= 26
+            elif 90 >= i and i >= 65 and i + n > 90:
+                i -= 26
+            answer += chr(i+n)
+        else:
+            answer += " "
+    return answer
+
+s="a B z" 
+n=4
+solution(s, n)
--- Programmers/문제풀이/고득점 kit/BFS-DFS/Level3_단어 변환.py
+++ Programmers/문제풀이/고득점 kit/BFS-DFS/Level3_단어 변환.py
@@ -2,23 +2,32 @@ from collections import deque
 
 def solution(begin, target, words):
     
+    # 타겟 단어가 words 에 없을 경우 0을 리턴한다.
     if target not in words:
         return 0
     
+    # l 은 words 의 총 갯수
     l = len(words)
+    # k 는 단어의 길이
     k = len(words[0])
     
+    # 방문여부를 확인하는 리스트
     visited = [False] * l
+    # deque
     q = deque()
     q.append((begin, 0))
     
     while q:
+        print(q)
         word, dist = q.popleft()
         
+        # 만약 현재 q의 word 와 target 이 동일하다면 dist 을 리넡
         if word == target:
             return dist
         
+        # l(words) 만큼 반복을 돌아주면서 
         for i in range(l):
+            # 방문한 기록이 없다면 
             if not visited[i]:
                 diff_count = 0
                 for j in range(k):
